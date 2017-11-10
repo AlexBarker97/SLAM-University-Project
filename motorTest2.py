5017c8220fb245d867a833ca4f1a1944813aaa72
Alex Barker <A.Barker.1@Warwick.ac.uk>
2017-11-10 09:50:06 +0000

    Update motorTest2.py

--- motorTest2.py
+++ motorTest2.py
@@ -1,52 +1,8 @@
-# Initio Motor Test
-# Moves: Forward, Reverse, turn Right, turn Left, Stop - then repeat
-# Press Ctrl-C to stop
-#
-# Also demonstrates writing to the LEDs
-#
-# To check wiring is correct ensure the order of movement as above is correct
-# Run using: sudo python motorTest.py
-
-
 import initio, time
 
-#======================================================================
-# Reading single character by forcing stdin to raw mode
-import sys
-import tty
-import termios
-
-def readchar():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    if ch == '0x03':
-        raise KeyboardInterrupt
-    return ch
-
-def readkey(getchar_fn=None):
-    getchar = getchar_fn or readchar
-    c1 = getchar()
-    if ord(c1) != 0x1b:
-        return c1
-    c2 = getchar()
-    if ord(c2) != 0x5b:
-        return c1
-    c3 = getchar()
-    return chr(0x10 + ord(c3) - 65)  # 16=Up, 17=Down, 18=Right, 19=Left arrows
-
-# End of single character reading
-#======================================================================
-
 speed = 30
-
 initio.init()
 
-# main loop
 try:
     while True:
         keyp=input("'w','z','s','a','>','<',' ':")
@@ -79,4 +35,3 @@ except KeyboardInterrupt:
 
 finally:
     initio.cleanup()
-    
