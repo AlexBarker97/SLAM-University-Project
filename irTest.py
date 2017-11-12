8168c6904d2ee39b1b93c550184e87d62b9426f3
Alex Barker <A.Barker.1@Warwick.ac.uk>
2017-11-12 12:17:28 +0000

    Update irTest.py

--- irTest.py
+++ irTest.py
@@ -3,31 +3,30 @@ import initio
 
 initio.init()
 
-
-
 try:
     lastL = initio.irLeft()
     lastR = initio.irRight()
-    lastLineL = initio.irLeftLine()
-    lastLineR = initio.irRightLine()
-    print ('Left, Right, LeftLine, RightLine:', lastL, lastR, lastLineL, lastLineR)
-    print()
+#    lastLineL = initio.irLeftLine()
+#    lastLineR = initio.irRightLine()
+#    print ('Left, Right, LeftLine, RightLine:', lastL, lastR, lastLineL, lastLineR)
+    print ('Left, Right:', lastL, lastR)
     while True:
         newL = initio.irLeft()
         newR = initio.irRight()
-        newLineL = initio.irLeftLine()
-        newLineR = initio.irRightLine()
-        if (newL != lastL) or (newR != lastR) or (newLineL != lastLineL) or (newLineR != lastLineR):
-            print ('Left, Right, LeftLine, RightLine:', lastL, lastR, lastLineL, lastLineR)
-            print()
-            lastL = newL
-            lastR = newR
-            lastLineL = newLineL
-            lastLineR = newLineR
+        print ('Left, Right:', newL, newR)
+#        newLineL = initio.irLeftLine()
+#        newLineR = initio.irRightLine()
+#        if (newL != lastL) or (newR != lastR) or (newLineL != lastLineL) or (newLineR != lastLineR):
+#            print ('Left, Right, LeftLine, RightLine:', lastL, lastR, lastLineL, lastLineR)
+#            print()
+#            lastL = newL
+#            lastR = newR
+#            lastLineL = newLineL
+#            lastLineR = newLineR
         time.sleep(0.1)
                           
 except KeyboardInterrupt:
-    print()
+    print('quitting')
 
 finally:
     initio.cleanup()
