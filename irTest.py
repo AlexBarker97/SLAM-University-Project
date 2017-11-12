0737fbcb10359ab27f0994792adff1c06eec995c
Alex Barker <A.Barker.1@Warwick.ac.uk>
2017-11-12 12:08:41 +0000

    Update irTest.py

--- irTest.py
+++ irTest.py
@@ -10,16 +10,16 @@ try:
     lastR = initio.irRight()
     lastLineL = initio.irLeftLine()
     lastLineR = initio.irRightLine()
-    print 'Left, Right, LeftLine, RightLine:', lastL, lastR, lastLineL, lastLineR
-    print
+    print ('Left, Right, LeftLine, RightLine:', lastL, lastR, lastLineL, lastLineR)
+    print()
     while True:
         newL = initio.irLeft()
         newR = initio.irRight()
         newLineL = initio.irLeftLine()
         newLineR = initio.irRightLine()
         if (newL != lastL) or (newR != lastR) or (newLineL != lastLineL) or (newLineR != lastLineR):
-            print 'Left, Right, LeftLine, RightLine:', lastL, lastR, lastLineL, lastLineR
-            print
+            print ('Left, Right, LeftLine, RightLine:', lastL, lastR, lastLineL, lastLineR)
+            print()
             lastL = newL
             lastR = newR
             lastLineL = newLineL
@@ -27,7 +27,7 @@ try:
         time.sleep(0.1)
                           
 except KeyboardInterrupt:
-    print
+    print()
 
 finally:
     initio.cleanup()
