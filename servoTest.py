68b8bfbf2d0d96704e75008ba3f45e503ed69162
Alex Barker <A.Barker.1@Warwick.ac.uk>
2017-11-12 12:51:43 +0000

    Update servoTest.py

--- servoTest.py
+++ servoTest.py
@@ -15,28 +15,28 @@ import sys
 import tty
 import termios
 
-def readchar():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    if ch == '0x03':
-        raise KeyboardInterrupt
-    return ch
-
-def readkey(getchar_fn=None):
-    getchar = getchar_fn or readchar
-    c1 = getchar()
-    if ord(c1) != 0x1b:
-        return c1
-    c2 = getchar()
-    if ord(c2) != 0x5b:
-        return c1
-    c3 = getchar()
-    return chr(0x10 + ord(c3) - 65)  # 16=Up, 17=Down, 18=Right, 19=Left arrows
+#def readchar():
+#    fd = sys.stdin.fileno()
+#    old_settings = termios.tcgetattr(fd)
+#    try:
+#        tty.setraw(sys.stdin.fileno())
+#        ch = sys.stdin.read(1)
+#    finally:
+#        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+#    if ch == '0x03':
+#        raise KeyboardInterrupt
+#    return ch
+#
+#def readkey(getchar_fn=None):
+#    getchar = getchar_fn or readchar
+#    c1 = getchar()
+#    if ord(c1) != 0x1b:
+#        return c1
+#    c2 = getchar()
+#    if ord(c2) != 0x5b:
+#        return c1
+#    c3 = getchar()
+#    return chr(0x10 + ord(c3) - 65)  # 16=Up, 17=Down, 18=Right, 19=Left arrows
 
 # End of single character reading
 #======================================================================
