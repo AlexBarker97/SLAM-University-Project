9331399c4c8fce4f48b2300149ce46bb9e1c2bd2
Alex Barker <A.Barker.1@Warwick.ac.uk>
2017-11-21 13:24:42 +0000

    Update and rename LiDARTest.py to lidarTest.py

---
+++ lidarTest.py
@@ -0,0 +1,44 @@
+import time
+import initio
+
+initio.init()
+
+
+def getDistance():
+    GPIO.setup(sonar, GPIO.OUT)
+    # Send 10us pulse to trigger
+    GPIO.output(sonar, True)
+    time.sleep(0.00001)
+    GPIO.output(sonar, False)
+    start = time.time()
+    count=time.time()
+    GPIO.setup(sonar,GPIO.IN)
+    while GPIO.input(sonar)==0 and time.time()-count<0.1:
+        start = time.time()
+    count=time.time()
+    stop=count
+    while GPIO.input(sonar)==1 and time.time()-count<0.1:
+        stop = time.time()
+    # Calculate pulse length
+    elapsed = stop-start
+    # Distance pulse travelled in that time is time
+    # multiplied by the speed of sound (cm/s)
+    distance = elapsed * 34000
+    # That was the distance there and back so halve the value
+    distance = distance / 2
+    return distance
+
+
+
+try:
+    while True:
+        dist = getDistance()
+        print "Distance: ", int(dist)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print()
+    pass
+
+finally:
+    initio.cleanup()
