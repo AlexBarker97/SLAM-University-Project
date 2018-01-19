ec635401b43bc577b1018db85bf9a7acd6cfb63a
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-19 11:42:57 +0000

    Update LidarUSBTests.py

--- LidarUSBTests.py
+++ LidarUSBTests.py
@@ -7,7 +7,6 @@ ser.parity = serial.PARITY_NONE
 ser.stopbits = serial.STOPBITS_ONE
 ser.bytesize = serial.EIGHTBITS
 ser.timeout = 10
-print(ser)
 ser.open()
 ser.write(bytes('F', 'UTF-8'))
 ser.write(bytes('T', 'UTF-8'))
