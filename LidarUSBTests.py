b8ea005ed8082c8b05802e513528f5a33ec38e6e
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-19 13:21:15 +0000

    Update LidarUSBTests.py

--- LidarUSBTests.py
+++ LidarUSBTests.py
@@ -16,18 +16,18 @@ hex = {"0":0,  "1":1,  "2":2,  "3":3,
        "8":8,  "9":9,  "a":10, "b":11,
        "c":12, "d":13, "e":14, "f":15}
 while True:
-  result = binascii.hexlify(ser.read(8))
-  result = str(result)
-  result = result.replace("b'", "")
-  result = result.replace("'", "")
-  print(result[4:12])
-  res1 = hex[result[4]]
-  res2 = hex[result[5]]
-  res3 = hex[result[6]]
-  res4 = hex[result[7]]
-  res5 = hex[result[8]]
-  res6 = hex[result[9]]
-  res7 = hex[result[10]]
-  res8 = hex[result[11]]
+       result = binascii.hexlify(ser.read(8))
+       result = str(result)
+       result = result.replace("b'", "")
+       result = result.replace("'", "")
+       print(result[4:12])
+       res1 = hex[result[4]]
+       res2 = hex[result[5]]
+       res3 = hex[result[6]]
+       res4 = hex[result[7]]
+       res5 = hex[result[8]]
+       res6 = hex[result[9]]
+       res7 = hex[result[10]]
+       res8 = hex[result[11]]
   if ((res1 == "5") and (res2 == "4")):
-  print(res3,res4,res5,res6,res7,res8)
+       print(res3,res4,res5,res6,res7,res8)
