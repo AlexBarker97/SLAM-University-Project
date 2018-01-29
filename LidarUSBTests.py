08c708a0c62bb48d50c6ef074a786960fe06501c
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-29 21:22:05 +0000

    Update LidarUSBTests.py

--- LidarUSBTests.py
+++ LidarUSBTests.py
@@ -10,7 +10,7 @@ ser.port = '/dev/ttyUSB0'
 ser.baudrate = 115200
 ser.parity = serial.PARITY_NONE
 ser.stopbits = serial.STOPBITS_ONE
-ser.bytesize = serial.EIGHTBITS
+ser.bytesize = serial.FOURBITS
 ser.timeout = 10
 ser.open()
 ser.write(bytes('P', 'UTF-8'))
