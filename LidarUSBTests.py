de3a3e5a86305fa82df075e8b022bfe2d0c8c2cf
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-29 21:33:05 +0000

    Update LidarUSBTests.py

--- LidarUSBTests.py
+++ LidarUSBTests.py
@@ -5,18 +5,20 @@ hex = {"0":0,  "1":1,  "2":2,  "3":3,
        "4":4,  "5":5,  "6":6,  "7":7,
        "8":8,  "9":9,  "a":10, "b":11,
        "c":12, "d":13, "e":14, "f":15}
-ser = serial.Serial()
-ser.close()
-ser.port = '/dev/ttyUSB0'
-ser.baudrate = 115200
-ser.parity = serial.PARITY_NONE
-ser.stopbits = serial.STOPBITS_ONE
-ser.bytesize = serial.EIGHTBITS
-ser.timeout = 10
-ser.open()
-ser.write(bytes('P', 'UTF-8'))
-ser.write(bytes('T', 'UTF-8'))
+def StartSer()
+       ser = serial.Serial()
+       ser.close()
+       ser.port = '/dev/ttyUSB0'
+       ser.baudrate = 115200
+       ser.parity = serial.PARITY_NONE
+       ser.stopbits = serial.STOPBITS_ONE
+       ser.bytesize = serial.EIGHTBITS
+       ser.timeout = 10
+       ser.open()
+       ser.write(bytes('P', 'UTF-8'))
+       ser.write(bytes('T', 'UTF-8'))
 
+StartSer()
 while True:
        value = 0
        result = binascii.hexlify(ser.read(4))
@@ -35,4 +37,7 @@ while True:
        res7 = hex[result[8]]
        if ((str(res1) == "5") and (str(res2) == "4")):
               value= value + (res3*(16**5)) + (res4*(16**4))
-       print(value/760)
+              print(value/760)
+       else:
+              StartSer()
+
