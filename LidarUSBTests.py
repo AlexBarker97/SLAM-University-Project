2e40b1755d9d44dc8abe17715f5c2fcf7db2c5b6
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-29 21:33:42 +0000

    Update LidarUSBTests.py

--- LidarUSBTests.py
+++ LidarUSBTests.py
@@ -5,7 +5,7 @@ hex = {"0":0,  "1":1,  "2":2,  "3":3,
        "4":4,  "5":5,  "6":6,  "7":7,
        "8":8,  "9":9,  "a":10, "b":11,
        "c":12, "d":13, "e":14, "f":15}
-def StartSer()
+def StartSer():
        ser = serial.Serial()
        ser.close()
        ser.port = '/dev/ttyUSB0'
