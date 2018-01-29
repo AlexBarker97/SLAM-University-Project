0ab9c80f16fc6666b626d0cca7e4df3e17a66833
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-29 21:20:51 +0000

    Update LidarUSBTests.py

--- LidarUSBTests.py
+++ LidarUSBTests.py
@@ -29,10 +29,6 @@ while True:
        res2 = hex[result[5]]
        res3 = hex[result[6]]
        res4 = hex[result[7]]
-       res5 = hex[result[8]]
-       res6 = hex[result[9]]
-       res7 = hex[result[10]]
-       res8 = hex[result[11]]
        if ((str(res1) == "5") and (str(res2) == "4")):
-              value= value + (res3*(16**5)) + (res4*(16**4)) + (res5*(16**3)) + (res6*(16**2)) + (res7*(16**1)) + (res8)
+              value= value + (res3*(16**5)) + (res4*(16**4))
        print(value/760)
