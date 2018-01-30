eeaf281946ee0c02ab33defdb641e94ebec0e612
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-30 22:20:25 +0000

    Update LidarUSBTests.py

--- LidarUSBTests.py
+++ LidarUSBTests.py
@@ -39,10 +39,8 @@ while True:
     print(res6)
     res7 = hex2dec[result[9]]
     print(res7)
-    res8 = hex2dec[result[10]]
-    print(res8)
     if ((str(res0) == "5") and (str(res1) == "4")):
-        value = value + (res2*(16**6)) + (res3*(16**5)) + (res4*(16**4)) + (res5*(16**3)) + (res6*(16**2)) + (res7*(16**1)) + (res7*(16**0))
+        value = value + (res2*(16**5)) + (res3*(16**4)) + (res4*(16**3)) + (res5*(16**2)) + (res6*(16**1)) + (res7*(16**0))
         value = value / 1000
         print(value)
     else:
