b6c174db576c70afcdad16b7fd0cf7824c5907d0
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-30 14:25:19 +0000

    Update LidarUSBTests.py

--- LidarUSBTests.py
+++ LidarUSBTests.py
@@ -1,7 +1,7 @@
 import serial
 import binascii
 
-hex = {"0":0,  "1":1,  "2":2,  "3":3,
+hex2dec = {"0":0,  "1":1,  "2":2,  "3":3,
        "4":4,  "5":5,  "6":6,  "7":7,
        "8":8,  "9":9,  "a":10, "b":11,
        "c":12, "d":13, "e":14, "f":15}
@@ -24,14 +24,14 @@ While True:
        value = 0
        result = binascii.hexlify(ser.read())
        result = str(result)
-       res0 = hex[result[2]]
-       res1 = hex[result[3]]
-       res2 = hex[result[4]]
-       res3 = hex[result[5]]
-       res4 = hex[result[6]]
-       res5 = hex[result[7]]
-       res6 = hex[result[8]]
-       res7 = hex[result[9]]
+       res0 = hex2dec[result[2]]
+       res1 = hex2dec[result[3]]
+       res2 = hex2dec[result[4]]
+       res3 = hex2dec[result[5]]
+       res4 = hex2dec[result[6]]
+       res5 = hex2dec[result[7]]
+       res6 = hex2dec[result[8]]
+       res7 = hex2dec[result[9]]
               if ((str(res0) == "5") and (str(res1) == "4")):
                      value = value + (res2*(16**6)) + (res3*(16**5)) + (res4*(16**4)) + (res5*(16**3)) + (res6*(16**2)) + (res7*(16**1)) + (res8*(16**0))
                      print(value/760)
