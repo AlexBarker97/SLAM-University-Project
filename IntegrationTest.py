b0a34dfcfd0894b35078e1817347c16102821975
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-30 22:46:09 +0000

    Update IntegrationTest.py

--- IntegrationTest.py
+++ IntegrationTest.py
@@ -1,6 +1,12 @@
 import serial, binascii, initio, time
 
+hex2dec = {"0": 0,  "1": 1,  "2": 2,  "3": 3,
+           "4": 4,  "5": 5,  "6": 6,  "7": 7,
+           "8": 8,  "9": 9,  "a": 10, "b": 11,
+           "c": 12, "d": 13, "e": 14, "f": 15}
+
 ser = serial.Serial()
+ser.close()
 ser.port = '/dev/ttyUSB0'
 ser.baudrate = 115200
 ser.parity = serial.PARITY_NONE
@@ -10,58 +16,41 @@ ser.timeout = 10
 ser.open()
 ser.write(bytes('P', 'UTF-8'))
 ser.write(bytes('T', 'UTF-8'))
-hex = {"0":0,  "1":1,  "2":2,  "3":3,
-       "4":4,  "5":5,  "6":6,  "7":7,
-       "8":8,  "9":9,  "a":10, "b":11,
-       "c":12, "d":13, "e":14, "f":15}
-initio.init()
-speed = 50
-try:
-       while True:
-              value = 0
-              result = binascii.hexlify(ser.read(8))
-              result = str(result)
-              result = result.replace("b'", "")
-              result = result.replace("'", "")
-              #print(result[4:12])
-              res1 = hex[result[4]]
-              res2 = hex[result[5]]
-              res3 = hex[result[6]]
-              res4 = hex[result[7]]
-              res5 = hex[result[8]]
-              res6 = hex[result[9]]
-              res7 = hex[result[10]]
-              res8 = hex[result[11]]
-              if ((str(res1) == "5") and (str(res2) == "4")):
-                     value= value + (res3*(16**5)) + (res4*(16**4)) + (res5*(16**3)) + (res6*(16**2)) + (res7*(16**1)) + (res8)
-              print(value/760)
-              keyp=input("'w','s','a','d','>','<',' ':")
-              if (keyp == 'w' or ord(keyp) == 16) and ((value/760) > 100):
-                     initio.forward(speed)
-                     print ('Forward', speed)
-              elif keyp == 'd' or ord(keyp) == 18:
-                     initio.spinRight(speed)
-                     print ('Spin Right', speed)
-              elif keyp == 'a' or ord(keyp) == 19:
-                     initio.spinLeft(speed)
-                     print ('Spin Left', speed)
-              elif keyp == '.' or keyp == '>':
-                     speed = min(100, speed+10)
-                     print ('Speed+', speed)
-              elif keyp == ',' or keyp == '<':
-                     speed = max (0, speed-10)
-                     print ('Speed-', speed)
-              elif keyp == ' ':
-                     initio.stop()
-                     print ('Stop')
-              elif keyp == "quit":
-                     break
-              else:
-                     initio.reverse(speed)
-                     print ('Wall Detected')
-            
-except KeyboardInterrupt:
-    print()
 
-finally:
-    initio.cleanup()
+while True:
+    state1 = 0
+    state2 = 0
+    value = 0
+    result = binascii.hexlify(ser.read(8))
+    result = str(result)
+    res0 = hex2dec[result[2]]
+    res1 = hex2dec[result[3]]
+    res2 = hex2dec[result[4]]
+    res3 = hex2dec[result[5]]
+    res4 = hex2dec[result[6]]
+    res5 = hex2dec[result[7]]
+    res6 = hex2dec[result[8]]
+    res7 = hex2dec[result[9]]
+    if ((str(res0) == "5") and (str(res1) == "4")):
+        value = value + (res2*(16**5)) + (res3*(16**4)) + (res4*(16**3)) + (res5*(16**2)) + (res6*(16**1)) + (res7*(16**0))
+        value = value / 1000
+        print(value)
+    else:
+        ser.close()
+        ser.port = '/dev/ttyUSB0'
+        ser.baudrate = 115200
+        ser.parity = serial.PARITY_NONE
+        ser.stopbits = serial.STOPBITS_ONE
+        ser.bytesize = serial.EIGHTBITS
+        ser.timeout = 10
+        ser.open()
+        ser.write(bytes('P', 'UTF-8'))
+        ser.write(bytes('T', 'UTF-8'))
+    speed = 100
+    if value >= 100:
+        initio.forward(speed)
+        print('Forward', speed)
+    else:
+        initio.reverse(speed)
+        print('Wall Detected')
+        
