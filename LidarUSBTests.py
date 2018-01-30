8cf1dd6e0d54ab399a0b68287aea6323acd63ea9
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-30 14:04:57 +0000

    Update LidarUSBTests.py

--- LidarUSBTests.py
+++ LidarUSBTests.py
@@ -6,23 +6,20 @@ hex = {"0":0,  "1":1,  "2":2,  "3":3,
        "8":8,  "9":9,  "a":10, "b":11,
        "c":12, "d":13, "e":14, "f":15}
 
-def StartSer():
-       ser = serial.Serial()
-       ser.close()
-       ser.port = '/dev/ttyUSB0'
-       ser.baudrate = 115200
-       ser.parity = serial.PARITY_NONE
-       ser.stopbits = serial.STOPBITS_ONE
-       ser.bytesize = serial.EIGHTBITS
-       ser.timeout = 10
-       ser.open()
-       ser.write(bytes('P', 'UTF-8'))
-       ser.write(bytes('T', 'UTF-8'))
+ser = serial.Serial()
+ser.close()
+ser.port = '/dev/ttyUSB0'
+ser.baudrate = 115200
+ser.parity = serial.PARITY_NONE
+ser.stopbits = serial.STOPBITS_ONE
+ser.bytesize = serial.EIGHTBITS
+ser.timeout = 10
+ser.open()
+ser.write(bytes('P', 'UTF-8'))
+ser.write(bytes('T', 'UTF-8'))
 
-while True:
-       StartSer()
-       res1 = "5"
-       res2 = "4"
+res1 = "5"
+res2 = "4"
        while ((str(res1) == "5") and (str(res2) == "4")):
               value = 0
               result = binascii.hexlify(ser.read())
@@ -40,8 +37,7 @@ while True:
               res6 = hex[result[7]]
               res7 = hex[result[8]]
               if ((str(res1) == "5") and (str(res2) == "4")):
-                     value= value + (res3*(16**5)) + (res4*(16**4))
+                     value = value + (res2*(16**6)) + (res3*(16**5)) + (res4*(16**4)) + (res5*(16**3)) + (res6*(16**2)) + (res7*(16**1)) + (res8*(16**0))
                      print(value/760)
               else:
                      StartSer()
-
