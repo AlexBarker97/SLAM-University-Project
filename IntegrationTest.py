352d0264d0c9f7c5495b9af55dbdd9f0bf9d8b9f
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-31 18:04:12 +0000

    Update IntegrationTest.py

--- IntegrationTest.py
+++ IntegrationTest.py
@@ -1,5 +1,5 @@
 import serial, binascii, initio, time
-
+ser.close()
 hex2dec = {"0": 0,  "1": 1,  "2": 2,  "3": 3,
            "4": 4,  "5": 5,  "6": 6,  "7": 7,
            "8": 8,  "9": 9,  "a": 10, "b": 11,
