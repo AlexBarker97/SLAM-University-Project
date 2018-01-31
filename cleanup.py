1d53cd8f5fcd612eeeb65fe300ade243ebbe733d
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-01-31 18:18:22 +0000

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,5 +1,11 @@
 import serial, initio
 ser = serial.Serial()
+ser.port = '/dev/ttyUSB0'
+ser.baudrate = 115200
+ser.parity = serial.PARITY_NONE
+ser.stopbits = serial.STOPBITS_ONE
+ser.bytesize = serial.EIGHTBITS
+ser.timeout = 10
 ser.close()
 initio.init()
 initio.cleanup()
