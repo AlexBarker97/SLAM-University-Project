f69f69617c24fcba6938b940252cdc32d8867f4e
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-02-06 00:30:41 +0000

    Update IntegrationSL.py

--- IntegrationSL.py
+++ IntegrationSL.py
@@ -66,7 +66,7 @@ while True:
         ser.open()
         ser.write(bytes('P', 'UTF-8'))
         ser.write(bytes('T', 'UTF-8'))
-for x in range(0, 63):
+for x in range(0, 65):
     print(r[x],theta[x])
 #ax = plt.subplot(111, projection='polar')
 #ax.plot(theta, r)
