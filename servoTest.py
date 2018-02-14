4ed11f5b78256f03e7c37fda3d30997c928b2550
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-02-14 16:48:58 +0000

    Update servoTest.py

--- servoTest.py
+++ servoTest.py
@@ -1,9 +1,7 @@
-import initio, time, RPi.GPIO as gpio, sys, tty, termios
+import RPi.GPIO as gpio, sys, tty, termios
 
 #pins
 pan = 22
-
-initio.init()
   
 gpio.setmode(gpio.BOARD)
 gpio.setup(pan, gpio.OUT)
@@ -15,27 +13,36 @@ while True:
     key = input("Use W=Up, S-Down, A-Left, D-Right, Space=Centre, 'quit','L','R'")
     if key == ' ':
         duty = 50
+        p.start(duty)
         p.ChangeDutyCycle(duty)
         print ("Centre")
+        p.stop()
     elif key.upper() == 'R':
         duty = 100
+        p.start(duty)
         p.ChangeDutyCycle(duty)
         print ("Right")
+        p.stop()
     elif key.upper() == 'L':
         duty = 0
+        p.start(duty)
         p.ChangeDutyCycle(duty)
         print ("Left")
+        p.stop()
     elif ((key == 'a') and (duty >= 5)):
         duty -= 5
+        p.start(duty)
         p.ChangeDutyCycle(duty)
         print ("Right", duty)
+        p.stop()
     elif ((key == 'd') and (duty <= 95)):
         duty += 5
+        p.start(duty)
         p.ChangeDutyCycle(duty)
         print ("Left", duty)
+        p.stop()
     elif key == 'quit':
         p.stop()
-        initio.cleanup()
         break
     else:
         print("incorrect input")
