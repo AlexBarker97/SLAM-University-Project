a5eac5dc9e2fa54da8964ddb8d31fdb41fd009fb
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-02-14 17:28:12 +0000

    Update servoTest.py

--- servoTest.py
+++ servoTest.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as gpio
+import RPi.GPIO as gpio, time
 
 #pins
 pan = 22
@@ -15,26 +15,31 @@ while True:
         duty = 50
         p.ChangeDutyCycle(duty)
         print ("Centre")
+        time.sleep(0.1)
         p.stop()
     elif key.upper() == 'R':
         duty = 75
         p.ChangeDutyCycle(duty)
         print ("Right")
+        time.sleep(0.1)
         p.stop()
     elif key.upper() == 'L':
         duty = 25
         p.ChangeDutyCycle(duty)
         print ("Left")
+        time.sleep(0.1)
         p.stop()
     elif ((key == 'a') and (duty >= 5)):
         duty -= 5
         p.ChangeDutyCycle(duty)
         print ("Right", duty)
+        time.sleep(0.1)
         p.stop()
     elif ((key == 'd') and (duty <= 95)):
         duty += 5
         p.ChangeDutyCycle(duty)
         print ("Left", duty)
+        time.sleep(0.1)
         p.stop()
     elif key == 'quit':
         p.stop()
