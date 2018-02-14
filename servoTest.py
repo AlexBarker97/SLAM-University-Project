f4d4d9133eba1721a64123e0da9b22ba551e0c6a
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-02-14 16:42:26 +0000

    Update servoTest.py

--- servoTest.py
+++ servoTest.py
@@ -1,45 +1,34 @@
 import initio, time, RPi.GPIO as gpio, sys, tty, termios
 
-#Motors
+#pins
 pan = 22
-tilt = 18
 
 initio.init()
-
-#def doServos():
-#    initio.setServo(pan, pVal)
-#    initio.setServo(tilt, tVal)
   
 gpio.setmode(gpio.BOARD)
 gpio.setup(pan, gpio.OUT)
 p = gpio.PWM(pan, 500)   # frequency is 500Hz, so each pulse is 2ms wide
 p.start(61)
-gpio.setup(tilt, gpio.OUT)
-t = gpio.PWM(tilt, 500)   # frequency is 500Hz, so each pulse is 2ms wide
-t.start(61)
-
-tilt = 61
-duty = 61
 
 while True:
     key = input("Use W=Up, S-Down, A-Left, D-Right, Space=Centre, 'quit','L','R'")
     if key == ' ':
-        duty = 61
+        duty = 50
         p.ChangeDutyCycle(duty)
-        print ("Centre: 61")
+        print ("Centre")
     elif key.upper() == 'R':
-        duty = 0
-        p.ChangeDutyCycle(duty)
-        print ("Right 25")
-    elif key.upper() == 'L':
         duty = 100
         p.ChangeDutyCycle(duty)
-        print ("Left 90")
-    elif ((key == 'd') and (duty >= 5)):
+        print ("Right")
+    elif key.upper() == 'L':
+        duty = 0
+        p.ChangeDutyCycle(duty)
+        print ("Left")
+    elif ((key == 'a') and (duty >= 5)):
         duty -= 5
         p.ChangeDutyCycle(duty)
         print ("Right", duty)
-    elif ((key == 'a') and (duty <= 95)):
+    elif ((key == 'd') and (duty <= 95)):
         duty += 5
         p.ChangeDutyCycle(duty)
         print ("Left", duty)
