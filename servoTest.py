4cc7a0bbc5844528cc39b958edd1281ba7430dcc
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-02-14 17:29:23 +0000

    Update servoTest.py

--- servoTest.py
+++ servoTest.py
@@ -10,37 +10,26 @@ duty = 50
 
 while True:
     key = input("Use W=Up, S-Down, A-Left, D-Right, Space=Centre, 'quit','L','R'")
-    p.start(50)
     if key == ' ':
         duty = 50
         p.ChangeDutyCycle(duty)
         print ("Centre")
-        time.sleep(0.1)
-        p.stop()
     elif key.upper() == 'R':
         duty = 75
         p.ChangeDutyCycle(duty)
         print ("Right")
-        time.sleep(0.1)
-        p.stop()
     elif key.upper() == 'L':
         duty = 25
         p.ChangeDutyCycle(duty)
         print ("Left")
-        time.sleep(0.1)
-        p.stop()
     elif ((key == 'a') and (duty >= 5)):
         duty -= 5
         p.ChangeDutyCycle(duty)
         print ("Right", duty)
-        time.sleep(0.1)
-        p.stop()
     elif ((key == 'd') and (duty <= 95)):
         duty += 5
         p.ChangeDutyCycle(duty)
         print ("Left", duty)
-        time.sleep(0.1)
-        p.stop()
     elif key == 'quit':
         p.stop()
         break
