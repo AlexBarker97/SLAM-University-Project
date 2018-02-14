daa2dcadd5322b93d9e4f6efc3d2c0481d1dab20
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-02-14 16:54:44 +0000

    Update servoTest.py

--- servoTest.py
+++ servoTest.py
@@ -7,37 +7,32 @@ gpio.setmode(gpio.BOARD)
 gpio.setup(pan, gpio.OUT)
 p = gpio.PWM(pan, 666.7)
 duty = 50
-p.start(duty)
 
 while True:
     key = input("Use W=Up, S-Down, A-Left, D-Right, Space=Centre, 'quit','L','R'")
+    p.start(50)
     if key == ' ':
         duty = 50
-        p.start(duty)
         p.ChangeDutyCycle(duty)
         print ("Centre")
         p.stop()
     elif key.upper() == 'R':
         duty = 100
-        p.start(duty)
         p.ChangeDutyCycle(duty)
         print ("Right")
         p.stop()
     elif key.upper() == 'L':
         duty = 0
-        p.start(duty)
         p.ChangeDutyCycle(duty)
         print ("Left")
         p.stop()
     elif ((key == 'a') and (duty >= 5)):
         duty -= 5
-        p.start(duty)
         p.ChangeDutyCycle(duty)
         print ("Right", duty)
         p.stop()
     elif ((key == 'd') and (duty <= 95)):
         duty += 5
-        p.start(duty)
         p.ChangeDutyCycle(duty)
         print ("Left", duty)
         p.stop()
