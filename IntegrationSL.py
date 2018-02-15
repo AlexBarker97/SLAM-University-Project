897d43c0130ccc98bce72e54c2e8b5823688ceaa
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-02-15 13:32:48 +0000

    Update IntegrationSL.py

--- IntegrationSL.py
+++ IntegrationSL.py
@@ -39,6 +39,9 @@ while True:
     res6 = hex2dec[result[8]]
     res7 = hex2dec[result[9]]
     if ((str(res0) == "5") and (str(res1) == "4")):
+        time.sleep(1)
+        p.ChangeFrequency(366)
+        p.start(duty)
         value = 0
         value = value + (res2*(16**5)) + (res3*(16**4)) + (res4*(16**3)) + (res5*(16**2)) + (res6*(16**1)) + (res7*(16**0))
         value = value/4250
@@ -49,9 +52,6 @@ while True:
             print(value)
             r.append(value)
             theta.append(duty)
-            time.sleep(1)
-            p.ChangeFrequency(366)
-            p.start(duty)
         else:
             break
     else:
