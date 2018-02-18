32c4d4478aca7e834ffe1d7445564759e350625c
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-02-18 23:19:55 +0000

    Update IntegrationSLT.py

--- IntegrationSLT.py
+++ IntegrationSLT.py
@@ -1,7 +1,15 @@
 import time, RPi.GPIO as gpio, numpy as np, matplotlib.pyplot as plt, serial, binascii, threading
 
+global r
+global theta
+global duty
+global p
+
 gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
+
+r = []
+theta = []
 gpio.setup(22, gpio.OUT) #pin 22 (panservo) set to output
 p = gpio.PWM(22, 366)
 duty = 90
@@ -12,39 +20,60 @@ hex2dec = {"0": 0,  "1": 1,  "2": 2,  "3": 3,
            "8": 8,  "9": 9,  "a": 10, "b": 11,
            "c": 12, "d": 13, "e": 14, "f": 15}
 
-ser = serial.Serial()
-ser.close()
-ser.port = '/dev/ttyUSB0'
-ser.baudrate = 115200
-ser.parity = serial.PARITY_NONE
-ser.stopbits = serial.STOPBITS_ONE
-ser.bytesize = serial.EIGHTBITS
-ser.timeout = 10
-ser.open()
-ser.write(bytes('P', 'UTF-8'))
-ser.write(bytes('T', 'UTF-8'))
+def setup():
+    ser = serial.Serial()
+    ser.close()
+    ser.port = '/dev/ttyUSB0'
+    ser.baudrate = 115200
+    ser.parity = serial.PARITY_NONE
+    ser.stopbits = serial.STOPBITS_ONE
+    ser.bytesize = serial.EIGHTBITS
+    ser.timeout = 10
+    ser.open()
+    ser.write(bytes('P', 'UTF-8'))
+    ser.write(bytes('T', 'UTF-8'))
+    while True:
+        result = binascii.hexlify(ser.read(8))
+        result = str(result)
+        res0 = hex2dec[result[2]]
+        res1 = hex2dec[result[3]]
+        res2 = hex2dec[result[4]]
+        res3 = hex2dec[result[5]]
+        res4 = hex2dec[result[6]]
+        res5 = hex2dec[result[7]]
+        res6 = hex2dec[result[8]]
+        res7 = hex2dec[result[9]]
+        if ((str(res0) == "5") and (str(res1) == "4")):
+            thread1 = threading.Thread(target=infiniteloop1)
+            thread1.start()
+            thread2 = threading.Thread(target=infiniteloop2)
+            thread2.start()
+            
+setup()
 
-global r
-global theta
-r = []
-theta = []
-
-def lidarReadings:
-  while True:
-
-while True:
-    result = binascii.hexlify(ser.read(8))
-    result = str(result)
-    res0 = hex2dec[result[2]]
-    res1 = hex2dec[result[3]]
-    res2 = hex2dec[result[4]]
-    res3 = hex2dec[result[5]]
-    res4 = hex2dec[result[6]]
-    res5 = hex2dec[result[7]]
-    res6 = hex2dec[result[8]]
-    res7 = hex2dec[result[9]]
-    if ((str(res0) == "5") and (str(res1) == "4")):
-        duty -= 1        
+def lidarReadings():
+    while True:
+        global value
+        value = 0
+        result = binascii.hexlify(ser.read(8))
+        result = str(result)
+        global res0
+        global res1
+        res0 = hex2dec[result[2]]
+        res1 = hex2dec[result[3]]
+        res2 = hex2dec[result[4]]
+        res3 = hex2dec[result[5]]
+        res4 = hex2dec[result[6]]
+        res5 = hex2dec[result[7]]
+        res6 = hex2dec[result[8]]
+        res7 = hex2dec[result[9]]
+        value = value + (res2*(16**5)) + (res3*(16**4)) + (res4*(16**3)) + (res5*(16**2)) + (res6*(16**1)) + (res7*(16**0))
+        value = value/4250
+    
+def setDuty():
+    while True:
+        if ((str(res0) == "5") and (str(res1) == "4")):
+        duty -= 1
         p.ChangeDutyCycle(duty)
         if duty >= 10:
             value = 0
@@ -54,6 +83,7 @@ while True:
             theta.append(duty)
         else:
             break
+                      
     else:
         ser.close()
         ser.port = '/dev/ttyUSB0'
