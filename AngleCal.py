c0def30577a4c4b2a0572d5242a1a17adf4a84bd
Alex Barker <A.Barker.1@Warwick.ac.uk>
2018-02-27 22:46:37 +0000

    Update AngleCal.py

--- AngleCal.py
+++ AngleCal.py
@@ -1,15 +1,4 @@
 import time, RPi.GPIO as gpio, numpy as np, matplotlib, serial, binascii, threading, math
-    
-
-
-        duty -= 1
-        p.ChangeDutyCycle(duty)
-        if duty >= 10:
-            r.append(value)
-            theta.append(((duty-10)*180)/80)
-            time.sleep(0.1)
-        else:
-            break
 
 gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
